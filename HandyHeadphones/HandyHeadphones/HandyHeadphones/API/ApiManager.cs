65565eb38f0e6457d7800827d5b9fd2702f3e348
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-02-02 19:09:30 -0600

    Implemented JA's API

---
+++ HandyHeadphones/HandyHeadphones/HandyHeadphones/API/ApiManager.cs
@@ -0,0 +1,29 @@
+﻿using HandyHeadphones.API.Interfaces;
+using StardewModdingAPI;
+
+namespace HandyHeadphones.API.Interfaces
+{
+    public static class ApiManager
+    {
+        private static IMonitor monitor = ModEntry.monitor;
+        private static IJsonAssetApi jsonAssetApi;
+
+        public static void HookIntoJsonAssets(IModHelper helper)
+        {
+            jsonAssetApi = helper.ModRegistry.GetApi<IJsonAssetApi>("spacechase0.JsonAssets");
+
+            if (jsonAssetApi is null)
+            {
+                monitor.Log("Failed to hook into spacechase0.JsonAssets.", LogLevel.Error);
+                return;
+            }
+
+            monitor.Log("Successfully hooked into spacechase0.JsonAssets.", LogLevel.Debug);
+        }
+
+        public static IJsonAssetApi GetJsonAssetInterface()
+        {
+            return jsonAssetApi;
+        }
+    }
+}
---
+++ HandyHeadphones/HandyHeadphones/HandyHeadphones/API/Interfaces/IJsonAssetApi.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HandyHeadphones.API.Interfaces
+{
+    public interface IJsonAssetApi
+    {
+        void LoadAssets(string path);
+    }
+}
