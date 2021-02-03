3e70a24b55138de003f9d64cd78ab75993d1c73c
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-02-02 21:47:42 -0600

    Overriding how GameLocation determines if MiniJukebox is playing

---
+++ HandyHeadphones/HandyHeadphones/HandyHeadphones/Patches/GameLocationPatch.cs
@@ -0,0 +1,37 @@
+﻿using Harmony;
+using StardewValley;
+using System.Reflection;
+using StardewModdingAPI;
+using Microsoft.Xna.Framework;
+using StardewValley.Menus;
+using System.Linq;
+using StardewValley.Objects;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace HandyHeadphones.Patches
+{
+	[HarmonyPatch]
+	class GameLocationPatch
+	{
+		private static IMonitor monitor = ModEntry.monitor;
+		private static IModHelper helper = ModEntry.modHelper;
+
+		internal static MethodInfo TargetMethod()
+		{
+			return AccessTools.Method(typeof(StardewValley.GameLocation), nameof(StardewValley.GameLocation.IsMiniJukeboxPlaying));
+		}
+
+		internal static bool Prefix(GameLocation __instance, ref bool __result)
+		{
+			Hat playerHat = Game1.player.hat;
+			if (playerHat != null && playerHat.Name == "Headphones" && __instance.miniJukeboxTrack.Value != "")
+            {
+				__result = true;
+				return false;
+            }
+
+			return true;
+		}
+	}
+}
