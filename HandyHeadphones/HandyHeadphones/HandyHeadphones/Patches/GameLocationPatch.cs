cc86136aa46e60a15f21ae6742d3f796793f03ba
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-02-04 11:55:39 -0600

    Added Studio Headphones as required condition

--- HandyHeadphones/HandyHeadphones/HandyHeadphones/Patches/GameLocationPatch.cs
+++ HandyHeadphones/HandyHeadphones/HandyHeadphones/Patches/GameLocationPatch.cs
@@ -25,7 +25,7 @@ namespace HandyHeadphones.Patches
 		internal static bool Prefix(GameLocation __instance, ref bool __result)
 		{
 			Hat playerHat = Game1.player.hat;
-			if (playerHat != null && (playerHat.Name == "Headphones" || playerHat.Name == "Earbuds") && __instance.miniJukeboxTrack.Value != "")
+			if (playerHat != null && (playerHat.Name == "Headphones" || playerHat.Name == "Earbuds" || playerHat.Name == "Studio Headphones"))
             {
 				__result = true;
 				return false;
